fc05e981ea4ecff4bd1636571059a126a49bec9a
20404wlgn <suil.s210340@ggh.goe.go.kr>
2022-09-22 21:35:39 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
+        title={'도움이 필요하신가요?'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
         ctaAction={goToShop}
